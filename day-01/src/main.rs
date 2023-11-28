e14532908b9ff44a8f6967a173a84fabfbd73887
Ian Pratt <ianjdpratt@gmail.com>
2023-11-28 15:39:25 +0000

    get ready for day 1!

---
+++ day-01/src/main.rs
@@ -0,0 +1,16 @@
+use advent_utils::macros::solution;
+
+fn main() {
+    part_1();
+    part_2();
+}
+
+#[solution(day = "01", part = "1")]
+fn part_1() -> usize {
+    0
+}
+
+#[solution(day = "01", part = "2")]
+fn part_2() -> usize {
+    0
+}

073687a6e20c87c225ad24c32603c61c5d3ef98c
Ian Pratt <ianjdpratt@gmail.com>
2023-12-01 23:48:37 -0600

    prep for day 3

---
+++ day-03/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    part_1();
+    part_2();
+}
+
+#[solution(day = "02", part = "1")]
+fn part_1(input: &str) -> u32 {
+    0
+}
+
+#[solution(day = "02", part = "2")]
+fn part_2(input: &str) -> u32 {
+    0
+}

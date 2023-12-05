9bdf2770cc2a671972264f50d799c51fcd1ef5e8
Ian Pratt <ianjdpratt@gmail.com>
2023-12-05 18:22:37 +0000

    this DOES NOT work! but progress!

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -10,19 +10,29 @@ fn main() {
 #[derive(Debug)]
 struct MapSegment {
     dst_start: i64,
+    dst_end: i64,
     src_start: i64,
-    length: i64,
+    src_end: i64,
 }
 
 impl MapSegment {
+    fn new_from_input(dst_start: i64, src_start: i64, length: i64) -> Self {
+        Self {
+            dst_start,
+            dst_end: dst_start + length - 1,
+            src_start,
+            src_end: src_start + length - 1,
+        }
+    }
+
     /// Check if a value is contained in the segment
     fn contains(&self, value: i64) -> bool {
-        value >= self.src_start && value < self.src_start + self.length
+        value >= self.src_start && value <= self.src_end
     }
 
     /// Map a value to the destination value
     fn map(&self, src: i64) -> i64 {
-        if src >= self.src_start && src < self.src_start + self.length {
+        if src >= self.src_start && src <= self.src_end {
             self.dst_start + src - self.src_start
         } else {
             src
@@ -65,12 +75,9 @@ fn parse_input(input: &str) -> (Vec<i64>, Vec<Mapping>) {
                     let dst_start = parts.next().unwrap().parse().unwrap();
                     let src_start = parts.next().unwrap().parse().unwrap();
                     let length = parts.next().unwrap().parse().unwrap();
-                    MapSegment {
-                        src_start,
-                        dst_start,
-                        length,
-                    }
+                    MapSegment::new_from_input(dst_start, src_start, length)
                 })
+                .sorted_by_key(|x| x.src_start)
                 .collect(),
         })
         .collect();
@@ -95,26 +102,76 @@ fn part_1(input: &str) -> i64 {
     min_location
 }
 
+impl Mapping {
+    /// Maps a seed range to a number of ranges
+    fn map_seed_range(&self, mut seed_range: (i64, i64)) -> Vec<(i64, i64)> {
+        let mut new_ranges: Vec<(i64, i64)> = vec![];
+
+        for splitter in &self.segments {
+            // If the seed range starts inside a segment
+            if seed_range.0 >= splitter.src_start {
+                // If the seed range is wholly inside the splitter, then we just append to the ranges and return.
+                if seed_range.1 <= splitter.src_end {
+                    new_ranges.push((splitter.map(seed_range.0), splitter.map(seed_range.1)));
+                    continue;
+                }
+                // Otherwise, we split into 2 segments: one we push (the one before, in the range), and one we loop on again
+                else {
+                    new_ranges.push((splitter.map(seed_range.0), splitter.map(splitter.src_end)));
+                    seed_range = (splitter.src_end + 1, seed_range.1);
+                }
+            }
+            // If the seed range starts outside a segment but ends inside/after one
+            else if seed_range.0 <= splitter.src_start {
+                // If it's not in the splitter, continue
+                if seed_range.1 <= splitter.src_start {
+                    continue;
+                }
+                // If the seed ends inside the splitter, we just make the 2 new seed ranges and return
+                else if seed_range.1 <= splitter.src_end {
+                    new_ranges.push((
+                        splitter.map(seed_range.0),
+                        splitter.map(splitter.src_start - 1),
+                    ));
+                    new_ranges.push((splitter.src_start, seed_range.1));
+                    continue;
+                }
+                // Otherwise, we split into 3 segments: the before, the inside, and the after (which we loop on again)
+                else {
+                    new_ranges.push((
+                        splitter.map(seed_range.0),
+                        splitter.map(splitter.src_start - 1),
+                    ));
+                    new_ranges.push((splitter.src_start, splitter.src_end));
+                    seed_range = (splitter.src_end + 1, seed_range.1);
+                }
+            }
+        }
+
+        new_ranges
+    }
+}
+
 #[solution(part = 2)]
 fn part_2(input: &str) -> i64 {
     let (seeds, mappings) = parse_input(input);
-    let seeds = seeds
+    let mut seed_ranges = seeds
         .iter()
         .tuples()
-        .flat_map(|(a, b)| *a..(a + b - 1))
+        .map(|(a, b)| (*a, (a + b)))
         .collect_vec();
 
-    seeds
-        .par_iter()
-        .map(|seed| {
-            let mut seed = *seed;
-            for mapping in &mappings {
-                let mapped = mapping.map(seed);
-                seed = mapped;
-            }
+    for mapping in &mappings {
+        let mut new_seeds = vec![];
 
-            seed
-        })
-        .min()
-        .unwrap()
+        for seed_range in seed_ranges {
+            new_seeds.append(&mut mapping.map_seed_range(seed_range))
+        }
+
+        seed_ranges = new_seeds;
+
+        seed_ranges.dedup();
+    }
+
+    seed_ranges.iter().sorted_by_key(|x| x.0).next().unwrap().0
 }

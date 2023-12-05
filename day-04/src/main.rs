85706393ff4b3db29d65a3c7f986c30677c7d975
Ian Pratt <ianjdpratt@gmail.com>
2023-12-04 21:09:22 -0600

    more optimizations

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -5,57 +5,37 @@ fn main() {
     part_2();
 }
 
+fn to_count(l: &str) -> u32 {
+    l.split(':')
+        .nth(1)
+        .unwrap()
+        .split('|')
+        .map(|s| {
+            s.split_whitespace()
+                .map(|s| s.trim().parse::<u32>().unwrap())
+                .fold(0_u128, |b, x| b | (1 << x))
+        })
+        .fold(u128::MAX, |a, x| a & x)
+        .count_ones()
+}
+
 #[solution(part = 1)]
 fn part_1(input: &str) -> i32 {
-    let mut score_sum = 0;
-
-    for line in input.lines() {
-        let nums = &line.split(':').collect::<Vec<_>>()[1..];
-        let split_pipe = nums[0].split('|').collect::<Vec<_>>();
-
-        let winning_set = split_pipe[0]
-            .split_whitespace()
-            .map(|s| s.trim().parse::<u32>().unwrap())
-            .fold(0_u128, |b, x| b | (1 << x));
-        let numbers_have = split_pipe[1]
-            .split_whitespace()
-            .map(|s| s.trim().parse::<u32>().unwrap())
-            .fold(0_u128, |b, x| b | (1 << x));
-        let total_winning_numbers = (winning_set & numbers_have).count_ones();
-
-        if total_winning_numbers > 0 {
-            score_sum += 2_i32.pow(total_winning_numbers - 1);
-        }
-    }
-
-    score_sum
+    input
+        .lines()
+        .map(to_count)
+        .fold(0, |a, x| if x > 0 { a + 2_i32.pow(x - 1) } else { a })
 }
 
 #[solution(part = 2)]
 fn part_2(input: &str) -> i32 {
-    let mut card_multipliers = vec![1; input.lines().count()];
+    let mut m = vec![1; input.lines().count()];
 
-    for (i, line) in input.lines().enumerate() {
-        let nums = &line.split(':').collect::<Vec<_>>()[1..];
-        let split_pipe = nums[0].split('|').collect::<Vec<_>>();
+    input
+        .lines()
+        .map(|l| to_count(l) as usize)
+        .enumerate()
+        .for_each(|(i, v)| (1..=v).for_each(|j| m[i + j] += m[i]));
 
-        let winning_set = split_pipe[0]
-            .split_whitespace()
-            .map(|s| s.trim().parse::<u32>().unwrap())
-            .fold(0_u128, |b, x| b | (1 << x));
-        let numbers_have = split_pipe[1]
-            .split_whitespace()
-            .map(|s| s.trim().parse::<u32>().unwrap())
-            .fold(0_u128, |b, x| b | (1 << x));
-        let total_winning_numbers = (winning_set & numbers_have).count_ones();
-
-        for j in 1..=total_winning_numbers as usize {
-            if i + j >= card_multipliers.len() {
-                break;
-            }
-            card_multipliers[i + j] += card_multipliers[i];
-        }
-    }
-
-    card_multipliers.iter().sum()
+    m.iter().sum()
 }

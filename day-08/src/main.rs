e6fb719c3c742524cee4c69a9223506214c77ec7
Ian Pratt <ianjdpratt@gmail.com>
2023-12-07 22:59:51 -0600

    prep day 8

---
+++ day-08/src/main.rs
@@ -0,0 +1,15 @@
+use advent_utils::macros::solution;
+
+fn main() {
+    part_1();
+    part_2();
+}
+#[solution(part = 1)]
+fn part_1(input: &str) -> u64 {
+    0
+}
+
+#[solution(part = 2)]
+fn part_2(input: &str) -> u64 {
+    0
+}

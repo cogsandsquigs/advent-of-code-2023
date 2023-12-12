76c2be025ac2cd2d0e35eaa85f2e33bce15c84bd
Ian Pratt <ianjdpratt@gmail.com>
2023-12-11 23:18:34 -0600

    upds to plan

--- day-12/src/main.rs
+++ day-12/src/main.rs
@@ -36,12 +36,10 @@ fn part_1(input: &str) -> u64 {
         // Idea: we count the number of chunks and the number of sizes. If they are equal, then we
         // have a 1:1 mapping between the two and can exploit that to calculate the number of ways
         // trivially. If they're not equal, then we have to do some more work.
-        // What's that work tho?
-        // We could work backwards, seeing what satisfies the constraints of the last chunk, and
-        // then work our way backwards from there. We could also work forwards, seeing what
-        // satisfies the constraints of the first chunk, and then work our way forwards from there.
-        // Either way would be tedious, because of the case where we have to satisfy multiple
-        // sizes with a single chunk, without knowing which chunk we're working with.
+        // How do we calculate the number of ways per chunk?
+        // We could just look at the number of possible sizes (both min and max), and then manually
+        // calculate the number of ways for each size. Will that scale? How will we do the manuall
+        // calculation?
 
         ways_sum += ways;
     }
